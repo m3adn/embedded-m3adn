87ee4098b718b8738018fc29f4208b82c1ecb3db
m3adn <dannyk98@gmail.com>
2023-05-24 18:42:14 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-
-import styles from '../page.module.css'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
      <h1>Hello World!</h1>
-
+     <a href="/about">Visit W3Schools</a> 
     </main>
   )
 }
