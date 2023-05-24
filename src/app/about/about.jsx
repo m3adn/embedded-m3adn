628dcaf97b99fcfe12f3c49c5e890eaef079cb80
m3adn <dannyk98@gmail.com>
2023-05-24 18:13:01 +0100

    About create

---
+++ src/app/about/about.jsx
@@ -0,0 +1,22 @@
+export default function About() {
+    return (
+        
+        <html lang="en">
+            <main className={styles.main}>
+            {
+
+            }
+
+            <head />
+            <nav>
+                <h1>HEEEY</h1>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </nav>
+            </main>
+        </html>
+    )
+}
