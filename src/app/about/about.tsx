5cbb22a9846cd208f3de5bf8285d9a7254318276
m3adn <dannyk98@gmail.com>
2023-05-24 18:53:08 +0100

    Update about.tsx

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function About() {
     return (
-        
-        <html lang="en">
             <main className={styles.main}>
         
             <head />
@@ -17,6 +16,6 @@ export default function About() {
                 </ul>
             </nav>
             </main>
-        </html>
+ 
     )
 }
